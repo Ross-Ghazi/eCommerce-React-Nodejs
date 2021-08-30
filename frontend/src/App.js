a57a3d601c0ad3b7d299fa568fe382fcdf0e4bcd
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-29 19:02:18 -0600

    end of 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
     <>
-      <h1> Hi there</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1> Hia there</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
