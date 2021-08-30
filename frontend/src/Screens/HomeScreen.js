a176d27fc388648c84fad630fe8006f99eff1abd
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-30 10:10:18 -0600

    end of 12, previous one was 11

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,17 +1,27 @@
-import React from "react";
-import products from "../products";
+import React, { useState, useEffect } from "react";
 import Product from "../Components/Product";
+import axios from "axios";
 
 import { Row, Col } from "react-bootstrap";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await axios.get("/api/products");
+      setProducts(res.data);
+      console.log(products);
+    };
+    fetchProduct();
+  }, []);
   return (
     <>
       <h1>Latest Prodcuts</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />{" "}
+            <Product product={product} />
           </Col>
         ))}
       </Row>
