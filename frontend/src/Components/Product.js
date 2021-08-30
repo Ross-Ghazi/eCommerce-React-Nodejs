4112c0032115dd4f733ba6606103dda76170e4c7
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-29 22:05:13 -0600

    end of 7

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import Rating from "../Components/Rating";
 
 function Product({ product }) {
   return (
@@ -16,9 +17,10 @@ function Product({ product }) {
             </Card.Title>
           </a>
           <Card.Text as="div">
-            <div className="my-3">
-              {product.rating} from {product.numReviews}
-            </div>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
           </Card.Text>
           <Card.Text as="h4">${product.price}</Card.Text>
         </Card.Body>
