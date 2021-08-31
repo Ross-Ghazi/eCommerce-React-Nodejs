680359d1a3ad42f97b56106e5abc6bd8038f3c34
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-08-30 18:57:54 -0600

    end of 42  exlcude [25-35]

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,29 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+//Register, public
+const reisterUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User alreasy exist");
+  }
+  const user = await User.create({ name, email, password });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user Data");
+  }
+});
+
 //get user profile, private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
@@ -37,4 +60,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, reisterUser };
