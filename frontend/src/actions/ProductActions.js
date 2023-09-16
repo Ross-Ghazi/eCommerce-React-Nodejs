12a0ab61a051657c25a3c21b5b7cafa2804c3237
Rossatama <ross.ghazi@gmail.com>
2023-09-15 22:36:00 -0600

    corrected spelling

--- frontend/src/actions/ProductActions.js
+++ frontend/src/actions/ProductActions.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 import {
-  PRODCUT_LIST_REQUEST,
-  PRODCUT_LIST_SUCCESS,
-  PRODCUT_LIST_FAIL,
-  PRODCUT_DETAIL_REQUEST,
-  PRODCUT_DETAIL_SUCCESS,
-  PRODCUT_DETAIL_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
   try {
-    dispatch({ type: PRODCUT_LIST_REQUEST });
+    dispatch({ type: PRODUCT_LIST_REQUEST });
     const url = "/api/products";
     const { data } = await axios.get(url);
-    dispatch({ type: PRODCUT_LIST_SUCCESS, payload: data });
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
-      type: PRODCUT_LIST_FAIL,
+      type: PRODUCT_LIST_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
@@ -27,14 +27,14 @@ export const listProducts = () => async (dispatch) => {
 
 export const listProductDetails = (id) => async (dispatch) => {
   try {
-    dispatch({ type: PRODCUT_DETAIL_REQUEST });
+    dispatch({ type: PRODUCT_DETAIL_REQUEST });
     const url = `/api/products/${id}`;
     const response = await fetch(url);
     const data = await response.json();
-    dispatch({ type: PRODCUT_DETAIL_SUCCESS, payload: data });
+    dispatch({ type: PRODUCT_DETAIL_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
-      type: PRODCUT_DETAIL_FAIL,
+      type: PRODUCT_DETAIL_FAIL,
       payload:
         error.response && error.response.data.message.detail
           ? error.response.data.message.detail
--- frontend/src/reducers/ProductRedcuers.js
+++ frontend/src/reducers/ProductRedcuers.js
@@ -1,18 +1,18 @@
 import {
-  PRODCUT_LIST_REQUEST,
-  PRODCUT_LIST_SUCCESS,
-  PRODCUT_LIST_FAIL,
-  PRODCUT_DETAIL_REQUEST,
-  PRODCUT_DETAIL_SUCCESS,
-  PRODCUT_DETAIL_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
 } from "../constants/productConstants";
 export const ProductListRedcuer = (state = { products: [] }, action) => {
   switch (action.type) {
-    case PRODCUT_LIST_REQUEST:
+    case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
-    case PRODCUT_LIST_SUCCESS:
+    case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
-    case PRODCUT_LIST_FAIL:
+    case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
@@ -24,11 +24,11 @@ export const ProductDetailRedcuer = (
   action
 ) => {
   switch (action.type) {
-    case PRODCUT_DETAIL_REQUEST:
+    case PRODUCT_DETAIL_REQUEST:
       return { ...state, loading: true };
-    case PRODCUT_DETAIL_SUCCESS:
+    case PRODUCT_DETAIL_SUCCESS:
       return { loading: false, product: action.payload };
-    case PRODCUT_DETAIL_FAIL:
+    case PRODUCT_DETAIL_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
